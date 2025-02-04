cda8e4612ffd8b1c33aecd4c4731ae8edf04bc43
boatDeckRoblox <106499693+boatDeckRoblox@users.noreply.github.com>
2025-02-04 14:07:27 -0500

    Update ModuleLoader.lua

--- ModuleLoader.lua
+++ ModuleLoader.lua
@@ -11,8 +11,8 @@ local Modules = {
 
 local function ModuleLoader:LoadModule(moduleToLoad)
   if table.find(Modules, module2Load) do
-    loadstring(game:HttpGet(githubStarter:gsub("ModName",moduleToLoad))
+    loadstring(game:HttpGet(githubStarter:gsub("ModName",moduleToLoad)))()
   end
 end
-    
+
 return ModuleLoader
