00b1aa6e975b85997acea05d6774bbba723ad507
boatDeckRoblox <106499693+boatDeckRoblox@users.noreply.github.com>
2025-02-04 14:12:01 -0500

    Update ModuleLoader.lua

--- ModuleLoader.lua
+++ ModuleLoader.lua
@@ -9,10 +9,25 @@ local Modules = {
 }
 
 
-local function ModuleLoader:LoadModule(moduleToLoad)
-      if table.find(Modules, moduleToLoad) then
+local ModuleLoader = {}
+
+local githubStarter = "https://raw.githubusercontent.com/boatDeckRoblox/script1/refs/heads/main/ModName"
+
+local Modules = {
+    "SwordModule",
+    "ExploiterModule",
+    "GameManager"  
+}
+
+function ModuleLoader:LoadModule(moduleToLoad)
+    if table.find(Modules, moduleToLoad) then
         return loadstring(game:HttpGet(githubStarter:gsub("ModName", moduleToLoad)))()
+    else
+        warn("Module " .. moduleToLoad .. " not found.")
     end
 end
+
+return ModuleLoader
+
 
 return ModuleLoader
